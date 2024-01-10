c2c46365efd5324a2208cab9a4f3d4868429ae63
nctycdsb <124855384+nctycdsb@users.noreply.github.com>
2024-01-10 13:40:29 -0500

    Created GetMouseButton in a Ball Class
    - Created LineRenderer: It allows to draw a line.
    - Created MousePosition: It is the ending point of the mouse.

--- Assets/C#/Ball.cs
+++ Assets/C#/Ball.cs
@@ -5,7 +5,10 @@ using System;
 
 public class Ball : MonoBehaviour
 {
+    public LineRenderer Lr;
     private Vector2 InitialMousePosition;
+    private Vector2 MousePosition;
+
 
     // Start is called before the first frame update
     //void Start()
@@ -15,7 +18,7 @@ public class Ball : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    { }
     public void UserInput()
     {
         if (Input.GetMouseButtonDown(0))
@@ -23,6 +26,14 @@ public class Ball : MonoBehaviour
             _hitMarble.Clear();
             SortedMarble.Clear();
             InitialMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Lr.positionCount = 2;
+            Lr.SetPosition(0, transform.position);
+            Lr.SetPosition(1, MousePosition);
         }
     }
 }
