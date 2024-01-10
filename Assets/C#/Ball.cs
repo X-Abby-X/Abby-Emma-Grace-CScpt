1e0a061664abb4811b1f720941cb6c4fa1313873
nctycdsb <124855384+nctycdsb@users.noreply.github.com>
2024-01-10 00:11:17 -0500

    Created UserInput In a Ball Class
    - Changed NewBehaviourScript --> Ball
    - Created mouse position variable
    - Created UserInput class: It is the starting point of the mouse

--- Assets/C#/Ball.cs
+++ Assets/C#/Ball.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Ball : MonoBehaviour
 {
+    private Vector2 InitialMousePosition;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //void Start()
+    //{
+
+    //}
 
     // Update is called once per frame
     void Update()
     {
-        
+    public void UserInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _hitMarble.Clear();
+            SortedMarble.Clear();
+            InitialMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
     }
 }
