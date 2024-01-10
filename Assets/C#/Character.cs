172966533f8d2880fbf2051a7cc84cab60125303
EmmaL123 <emmajade.lee@gmail.com>
2024-01-10 18:14:28 -0500

    Updated Character script (+ ChooseEnemy, TakeDamage methods)
    - created a ChooseEnemy method so that the player can select enemies based on conditions relating to colour
    - created a TakeDamage Method to handle the player's health and defeat, created a _applied attribute to keep track of if a power up is being used or not

--- Assets/C#/Character.cs
+++ Assets/C#/Character.cs
@@ -9,6 +9,7 @@ public class Character : Person
     public int MaxHealth;
     public Ball ball;
     private bool _bonus = false;
+    public bool _applied = false;
 
     public Character(string colour, int health, int strength) : base(colour, health, strength)
     {
@@ -32,5 +33,49 @@ public class Character : Person
 
         targetEnemy.GetComponent<Enemy>().TakeDamage(finalDamage);
         Debug.Log($"targetEnemy health : {targetEnemy.GetComponent<Enemy>().Health}");
+    }
+
+    public GameObject ChooseEnemy(Dictionary<string, List<GameObject>> ColourEnemylist)
+    {
+        foreach (KeyValuePair<string, List<GameObject>> kvp in ColourEnemylist)
+        {
+            //Debug.Log($"character colour  {this.Colour}, enemy colour {kvp.Key}");
+            if (this.Colour == "Red" && kvp.Key == "Yellow")
+            {
+                _bonus = true;
+                return kvp.Value[0];
+            }
+            else if (this.Colour == "Yellow" && kvp.Key == "Blue")
+            {
+                _bonus = true;
+                return kvp.Value[0];
+            }
+            else if (this.Colour == "Blue" && kvp.Key == "Red")
+            {
+                _bonus = true;
+                return kvp.Value[0];
+            }
+            else
+            {
+                _bonus = false;
+            }
+        }
+        return MarbleGameController.EnemyList[0];
+    }
+
+    public override void TakeDamage(int damage)
+    {
+        this.Health -= damage;
+        if (this.Health <= 0)
+        {
+            ApplyHealthPowerUp();
+            if (this._applied == false)
+            {
+                this.Health = 0;
+                this.IsDefeated();
+            }
+        }
+        _applied = false;
+        MarbleGameController.updateValues();
     }
 }
