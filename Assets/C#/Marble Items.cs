03e3c62f32fdadde73654375ee40cde4430c6d8f
nctycdsb <124855384+nctycdsb@users.noreply.github.com>
2024-01-15 12:59:52 -0500

    Created MarbleItems script
    - Created MarbleItems Class that inherits from Items
    - Created constructor to initialize the level, type, name, cost, and colour
    - Created Ability to calculate stats

---
+++ Assets/C#/Marble Items.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarbleItems : Items
+{
+    public int ColourValue;
+    public MarbleItems(int level, string type, string name, int cost, string colour) : base(level, type, name, cost)
+    {
+        if (colour == "red")
+        {
+            this.ColourValue = 1;
+        }
+        else if (colour == "yellow")
+        {
+            this.ColourValue = 2;
+        }
+        if (colour == "blue")
+        {
+            this.ColourValue = 3;
+        }
+
+    }
+
+    public float Ability(float stats)
+    {
+        return stats * 2;
+    }
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    // Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+}
