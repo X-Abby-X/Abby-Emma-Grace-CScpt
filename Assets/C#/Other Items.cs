1f1daada0434b6bf04d7ac9b438259e59055fdd2
nctycdsb <124855384+nctycdsb@users.noreply.github.com>
2024-01-15 13:04:25 -0500

    Created Other Items script
    - Created OtherItems class that inherits from Items
    - Created a constructor to initialize the level, type, name, and cost

---
+++ Assets/C#/Other Items.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherItems : Items
+{
+    public OtherItems(int level, string type, string name, int cost) : base(level, type, name, cost)
+    {
+
+    }
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+}
