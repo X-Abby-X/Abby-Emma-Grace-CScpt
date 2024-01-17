39fe659c8a00576179eed69a124e3b711e2cfb85
EmmaL123 <emmajade.lee@gmail.com>
2024-01-17 13:14:03 -0500

    Added OnDisable method to Enemy Script
    - When enemy is defeated/disabled, it is removed from the enemy list.
    - remove their hp textbox

--- Assets/C#/Marble Game/Enemy.cs
+++ Assets/C#/Marble Game/Enemy.cs
@@ -18,4 +18,30 @@ public class Enemy : Person
         character.GetComponent<Character>().TakeDamage(damage);
     }
 
+    private void OnDisable()
+    {
+        if (MarbleGameController.TextList.Count <= 1)
+        {
+            MarbleGameController.TextList[0].enabled = false;
+            MarbleGameController.TextList.RemoveAt(0);
+        }
+        else
+        {
+            int index = 0;
+            for (int i = 0; i < MarbleGameController.EnemyList.Count; i++)
+            {
+                if (MarbleGameController.EnemyList[i] == this.gameObject)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            MarbleGameController.TextList[index + 1].enabled = false;
+            MarbleGameController.TextList.RemoveAt(index + 1);
+        }
+
+        MarbleGameController.EnemyList.Remove(this.gameObject);
+        MarbleGameController.ColourEnemylist.Clear();
+        MarbleGameController.SortEnemy(MarbleGameController.EnemyList);
+    }
 }
