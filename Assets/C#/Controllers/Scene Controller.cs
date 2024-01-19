ad163ad2ed1ae7319abbefdedab5620b191bb7b8
X-Abby-X <abbyabbylui@gmail.com>
2024-01-19 17:19:47 -0500

    added background music

--- Assets/C#/Controllers/Scene Controller.cs
+++ Assets/C#/Controllers/Scene Controller.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 public class SceneController : MonoBehaviour
 {
 
+    public AudioSource MusicSource;
+    public AudioClip BackGroundMusic;
     public Vector2 PlayerOnMapPosition;
     public List<bool> StoreButtonActiveList = new List<bool>();
     public Player Player;
@@ -26,7 +28,8 @@ public class SceneController : MonoBehaviour
         }
 
         DontDestroyOnLoad(this.gameObject);
-
+        MusicSource.clip = BackGroundMusic;
+        MusicSource.Play();
     }
 
     void Update()
